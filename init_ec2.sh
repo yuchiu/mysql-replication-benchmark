9629f57b8c637ff2cb77a22054bceb8cf174f437
yuchiu <yuchiu2002@hotmail.com>
2019-04-13 23:37:46 +0000

    added tomcat installation

--- init_ec2.sh
+++ init_ec2.sh
@@ -12,3 +12,10 @@ echo "mysql-server mysql-server/root_password_again password root" | sudo debcon
 sudo apt-get -y install mysql-server mysql-client
 sudo systemctl start mysql
 sudo systemctl enable mysql
+
+# tomcat
+sudo sed -i '2 i deb http://us.archive.ubuntu.com/ubuntu/ xenial main' -i /etc/apt/sources.list
+sudo sed -i '3 i deb http://us.archive.ubuntu.com/ubuntu/ xenial universe' -i /etc/apt/sources.list
+sudo sed -i 's:# JAVA_HOME=/usr/lib/jvm/java-6-openjdk/bin/java:JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre:g' /etc/default/tomcat7
+sudo apt-get update
+sudo apt-get install -y tomcat7
