af7c189e3ea7e6713c5ba318eaebeec50f09836d
Yu Chiu <yuchiu2002@hotmail.com>
2019-04-15 10:48:55 -0400

    updated JAVA_HOME ref after tomcat is installed

--- init_ec2.sh
+++ init_ec2.sh
@@ -16,6 +16,6 @@ sudo systemctl enable mysql
 # tomcat
 sudo sed -i '2 i deb http://us.archive.ubuntu.com/ubuntu/ xenial main' -i /etc/apt/sources.list
 sudo sed -i '3 i deb http://us.archive.ubuntu.com/ubuntu/ xenial universe' -i /etc/apt/sources.list
-sudo sed -i 's:# JAVA_HOME=/usr/lib/jvm/java-6-openjdk/bin/java:JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre:g' /etc/default/tomcat7
 sudo apt-get update
 sudo apt-get install -y tomcat7
+sudo sed -i 's:# JAVA_HOME=/usr/lib/jvm/java-6-openjdk/bin/java:JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre:g' /etc/default/tomcat7
